f3c1b340d00e7df5a6f16ec699b26c3a8a6b0cfc
wrenjohnson <wrenjohnson22@gmail.com>
2023-07-31 20:38:56 -0400

    wrote code for generateMarkdown.js

---
+++ generateMarakdown.js
@@ -0,0 +1,41 @@
+// Creating a function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+    if (license !== "none") {
+      return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    }
+    return "";
+  }
+  
+  //Creating a function to generate markdown for README
+  function generateMarkdown(data) {
+    return `# ${data.title} 
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  ## Deployed Application URL
+  ${data.link}
+  ## Screenshot
+  ![alt-text](${data.screenshot})
+  ## Table of Contents
+  * [Features](#features)
+  * [Languages & Dependencies](#languagesanddependencies)
+  * [How to Use This Application](#HowtoUseThisApplication)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  ## Features
+  ${data.features}
+  ## Languages & Dependencies
+  ${data.require}
+  ## How to Use This Application:
+  ${data.usage}
+  ## Contributors
+  ${data.contributors}
+  ## Testing
+  ${data.test}
+  ## Questions
+  Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
+  `;
+  }
+  
+  module.exports = generateMarkdown;
